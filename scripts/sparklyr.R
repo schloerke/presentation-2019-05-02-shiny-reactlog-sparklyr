46dcfc2691650729a96e6760c6b0ab6b47f841b6
Barret Schloerke <schloerke@gmail.com>
2019-05-01 14:21:46 -0400

    init

---
+++ scripts/sparklyr.R
@@ -0,0 +1,209 @@
+# install.packages("sparklyr")
+
+
+library(dplyr)
+library(sparklyr)
+spark_install()
+
+
+sc <- spark_connect(master = "local")
+
+# get data
+remotes::install_cran(c("nycflights13", "Lahman"))
+
+
+# copy data in
+iris_tbl <- copy_to(sc, iris)
+flights_tbl <- copy_to(sc, nycflights13::flights, "flights")
+batting_tbl <- copy_to(sc, Lahman::Batting, "batting")
+
+# look at src tables
+src_tbls(sc)
+
+# display top 10 flights with a delay of 2 minutes
+flights_tbl %>% filter(dep_delay == 2)
+
+
+# get all unique tail numbers count, avg distance, and delay
+delay <-
+  flights_tbl %>%
+  group_by(tailnum) %>%
+  summarise(
+    count = n(),
+    dist = mean(distance, na.rm = TRUE),
+    delay = mean(arr_delay, na.rm = TRUE)) %>%
+  filter(
+    count > 10,
+    dist < 2000,
+    !is.na(delay)
+  ) %>%
+  collect()
+
+# display all unique delay information
+delay
+
+# plot delays
+library(ggplot2)
+ggplot(delay, aes(dist, delay)) +
+  geom_point(aes(size = count), alpha = 1/2) +
+  geom_smooth() +
+  scale_size_area(max_size = 2)
+
+
+
+
+
+# get all flight delays, update month and day, select certain columns
+df <- flights_tbl %>%
+  filter(!is.na(dep_delay)) %>%
+  mutate(
+    month = paste0("m", month),
+    day = paste0("d", day)
+  ) %>%
+  select(dep_delay, sched_dep_time, month, day, distance)
+
+
+
+# # look at sql code being generated for execution
+# ft_dplyr_transformer(sc, df) %>%
+#   ml_param("statement") %>%
+#   cat()
+
+
+# The following step will create a 5 stage pipeline:
+#
+# 1. SQL transformer - Resulting from the ft_dplyr_transformer() transformation
+# 2. Binarizer - To determine if the flight should be considered delay. The eventual outcome variable.
+# 3. Bucketizer - To split the day into specific hour buckets
+# 4. R Formula - To define the model’s formula
+# 5. Logistic Model
+flights_pipeline <-
+  ml_pipeline(
+    sc) %>%
+  ft_dplyr_transformer(
+    tbl = df) %>%
+  ft_binarizer(
+    input_col = "dep_delay",
+    output_col = "delayed",
+    threshold = 15) %>%
+  ft_bucketizer(
+    input_col = "sched_dep_time",
+    output_col = "hours",
+    splits = c(400, 800, 1200, 1600, 2000, 2400)) %>%
+  ft_r_formula(
+    delayed ~ month + day + hours + distance) %>%
+  ml_logistic_regression()
+
+
+flights_pipeline
+
+
+
+# Partition the data into training and testing
+partitioned_flights <-
+  sdf_partition(
+    flights_tbl,
+    training = 0.1,
+    testing = 0.1,
+    rest = 0.8)
+
+# Get Predictions from model fit
+predictions <-
+  flights_pipeline %>%
+  ml_fit(partitioned_flights$training)
+  ml_transform(partitioned_flights$testing)
+predictions
+
+
+prediction_counts <-
+  predictions %>%
+  group_by(delayed, prediction) %>%
+  tally() %>%
+  collect() %>%
+  mutate(
+    perc = round(n / sum(n) * 100)
+  ) %>%
+  merge(
+    tribble(
+      ~correct, ~delayed, ~prediction,
+      TRUE, 0, 0,
+      TRUE, 1, 1,
+      FALSE, 1, 0,
+      FALSE, 0, 1
+    )
+  ) %>%
+  arrange(desc(correct), desc(n))
+prediction_counts
+
+# Area under ROC
+ml_binary_classification_evaluator(predictions)
+# RMSE
+ml_regression_evaluator(predictions)
+
+
+
+# # Save pipelines
+# ml_save(
+#   flights_pipeline,
+#   "flights_pipeline",
+#   overwrite = TRUE
+# )
+# ml_save(
+#   fitted_pipeline,
+#   "fitted_pipeline",
+#   overwrite = TRUE
+# )
+#
+#
+#
+# # ----------------------------------
+#
+# # after some time....
+#
+# library(sparklyr)
+# library(dplyr)
+# sc <- spark_connect(master = "local")
+#
+# # reload the model from disk
+# reloaded_fitted_pipeline <- ml_load(sc, "fitted_pipeline")
+#
+# flights_tbl <- copy_to(sc, nycflights13::flights, "flights")
+#
+# partitioned_flights <-
+#   sdf_partition(
+#     flights_tbl,
+#     training = 0.1,
+#     testing = 0.1,
+#     rest = 0.8)
+#
+# predictions <-
+#   ml_transform(
+#     reloaded_fitted_pipeline,
+#     partitioned_flights$testing)
+# predictions
+#
+#
+# prediction_counts <-
+#   predictions %>%
+#   group_by(delayed, prediction) %>%
+#   tally() %>%
+#   collect() %>%
+#   mutate(
+#     perc = round(n / sum(n) * 100)
+#   ) %>%
+#   merge(
+#     tribble(
+#       ~correct, ~delayed, ~prediction,
+#       TRUE, 0, 0,
+#       TRUE, 1, 1,
+#       FALSE, 1, 0,
+#       FALSE, 0, 1
+#     )
+#   ) %>%
+#   arrange(desc(correct), desc(n))
+# prediction_counts
+#
+# # Area under ROC
+# ml_binary_classification_evaluator(predictions)
+# # RMSE
+# ml_regression_evaluator(predictions)
---
+++ scripts/sparklyr_shiny_app.R
@@ -0,0 +1,154 @@
+
+
+library(sparklyr)
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+
+ui <- fluidPage(
+
+  titlePanel("Sparklyr demo"),
+
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("count", "Min count:", 10, 400, value = 10),
+      sliderInput("dist", "Distance:", 0, 2000, value = c(0,2000)),
+      sliderInput("threshold", "Delay threshold:", 1, 40, value = 15),
+      sliderInput("training", "Testing Percentage:", 1, 10, value = 2),
+      sliderInput("testing", "Training Percentage:", 1, 10, value = 2)
+    ),
+    mainPanel(
+      plotOutput("plot"),
+      div("Model predictions: ", tableOutput("table")),
+      div("AOC: ", verbatimTextOutput("aoc")),
+      div("RMSE: ", verbatimTextOutput("rmse"))
+    )
+  )
+)
+
+sc <- spark_connect(master = "local")
+
+# copy data in
+flights_tbl <- copy_to(sc, nycflights13::flights, "flights", overwrite = TRUE)
+
+server <- function(input, output) {
+
+  delay_data <- reactive({
+    # get all unique tail numbers count, avg distance, and delay
+    delay <-
+      flights_tbl %>%
+      group_by(tailnum) %>%
+      summarise(
+        count = n(),
+        dist = mean(distance, na.rm = TRUE),
+        delay = mean(arr_delay, na.rm = TRUE)) %>%
+      filter(
+        count > input$count,
+        dist <= input$dist[2],
+        dist >= input$dist[1],
+        !is.na(delay)
+      ) %>%
+      collect()
+
+    # display all unique delay information
+    delay
+
+  })
+  output$plot <- renderPlot({
+    # plot delays
+    delay_data() %>%
+      ggplot(aes(dist, delay)) +
+        geom_point(aes(size = count), alpha = 1/2) +
+        geom_smooth() +
+        scale_size_area(max_size = 2)
+  })
+
+
+
+  # get all flight delays, update month and day, select certain columns
+  df <- flights_tbl %>%
+    filter(!is.na(dep_delay)) %>%
+    mutate(
+      month = paste0("m", month),
+      day = paste0("d", day)
+    ) %>%
+    select(dep_delay, sched_dep_time, month, day, distance)
+
+
+  # The following step will create a 5 stage pipeline:
+  #
+  # 1. SQL transformer - Resulting from the ft_dplyr_transformer() transformation
+  # 2. Binarizer - To determine if the flight should be considered delay. The eventual outcome variable.
+  # 3. Bucketizer - To split the day into specific hour buckets
+  # 4. R Formula - To define the model’s formula
+  # 5. Logistic Model
+  reactive_flights_pipeline <- reactive({
+    sc %>%
+      ml_pipeline() %>%
+      ft_dplyr_transformer(
+        tbl = df) %>%
+      ft_binarizer(
+        input_col = "dep_delay",
+        output_col = "delayed",
+        threshold = input$threshold) %>%
+      ft_bucketizer(
+        input_col = "sched_dep_time",
+        output_col = "hours",
+        splits = c(400, 800, 1200, 1600, 2000, 2400)) %>%
+      ft_r_formula(
+        delayed ~ month + day + hours + distance) %>%
+      ml_logistic_regression()
+  })
+
+
+  # Partition the data into training and testing
+  partitioned_flights <- reactive({
+    sdf_partition(
+      flights_tbl,
+      training = input$training / 100,
+      testing = input$testing / 100,
+      rest = (100 - input$testing - input$training) / 100)
+  })
+
+  # Get Predictions from model fit
+  predictions <- reactive({
+    reactive_flights_pipeline() %>%
+    ml_fit(
+      partitioned_flights()$training) %>%
+    ml_transform(
+      partitioned_flights()$testing)
+  })
+
+  output$table <- renderTable({
+    predictions() %>%
+      group_by(delayed, prediction) %>%
+      tally() %>%
+      collect() %>%
+      mutate(
+        perc = round(n / sum(n) * 100)
+      ) %>%
+      merge(
+        tribble(
+          ~correct, ~delayed, ~prediction,
+          TRUE, 0, 0,
+          TRUE, 1, 1,
+          FALSE, 1, 0,
+          FALSE, 0, 1
+        )
+      ) %>%
+      arrange(desc(correct), desc(n))
+  })
+
+  # Area under ROC
+  output$aoc <- renderText({
+    ml_binary_classification_evaluator(predictions())
+  })
+  # RMSE
+  output$rmse <- renderText({
+    ml_regression_evaluator(predictions())
+  })
+
+}
+
+shinyApp(ui, server)
